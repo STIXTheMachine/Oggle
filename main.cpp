5bc8fdc38124a33647e9c45deeae8dfce7e20d3b
Jake S. <jake@studio346.net>
2025-10-12 20:48:33 -0500

    Add GLBinding

--- main.cpp
+++ main.cpp
@@ -1,7 +1,64 @@
 #include <iostream>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glbinding/gl/gl.h>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+#include "glbinding/glbinding.h"
+using namespace gl;
+
+struct GLFWLib {
+    static void Init() {
+        static GLFWLib instance;
+    }
+
+    static bool isInitialized() {
+        return glfwInit() == GLFW_TRUE;
+    }
+
+    ~GLFWLib() {
+        glfwTerminate();
+    }
+
+private:
+    GLFWLib() {
+        glfwInit();
+    }
+};
+
+// Anonymous enum inside namespace to preserve scoping without having to static_cast<int>(ReturnValues::Foo) everywhere.
+namespace ReturnValues {
+    enum {
+        SUCCESS = 0,
+        GLFW_INIT_FAILED,
+        GLFW_MAIN_WINDOW_CREATION_FAILED,
+    };
+}
+
 int main() {
 
-    return 0;
+    GLFWLib::Init();
+
+    if (!GLFWLib::isInitialized()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return ReturnValues::GLFW_INIT_FAILED;
+    }
+
+    auto MainWindow = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+    if (!MainWindow) {
+        return ReturnValues::GLFW_MAIN_WINDOW_CREATION_FAILED;
+    }
+
+    glfwMakeContextCurrent(MainWindow);
+
+    glbinding::initialize(glfwGetProcAddress);
+
+    while (!glfwWindowShouldClose(MainWindow)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(MainWindow);
+
+        glfwPollEvents();
+    }
+
+    return ReturnValues::SUCCESS;
 }
