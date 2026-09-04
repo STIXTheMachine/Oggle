37a6eba76e90418fa8a9f61f0247055382691786
Jake <jake@superiorvalves.com>
2026-09-04 11:50:55 -0500

    Add test to sandbox, currently have linker errors to be resolved

--- Projects/Scratch/Source/Private/main.cpp
+++ Projects/Scratch/Source/Private/main.cpp
@@ -1,6 +1,7 @@
-#include "Core/Utilities/Assert.hpp"
+#include "Core/Logging/Logging.hpp"
 
 int main()
 {
-
+    Oggle::BasicFileSink TempSink { "Temp", "Temp" };
+    Oggle::Private::LogImplNew("NotACategory", Oggle::ELogVerbosity::Info, Oggle::ELogVerbosity::Info, Oggle::ELogSinks::Default, TempSink, "Hello, World!");
 }
