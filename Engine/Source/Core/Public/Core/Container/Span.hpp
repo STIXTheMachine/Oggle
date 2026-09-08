e6dea8df836bc5b7b962bb0a01ae15f0dc20b3e5
Jake <jake@superiorvalves.com>
2026-09-08 11:41:10 -0500

    Add Count() and Data() member functions to Span<T>

--- Engine/Source/Core/Public/Core/Container/Span.hpp
+++ Engine/Source/Core/Public/Core/Container/Span.hpp
@@ -34,6 +34,14 @@ namespace Oggle
         /// @return An optional object which contains a const reference to the element at Index if Index is valid, empty otherwise
         Optional<const T&> operator[](size_t Index) const;
 
+        /// @brief Get the number of objects held by the Span
+        /// @return Number of objects
+        size_t Count() const;
+
+        /// @brief Get a raw pointer to the data held by the Span
+        /// @return Pointer to first element
+        T* Data() const;
+
         using Iterator             =       T*;
         using ConstIterator        = const T*;
         using ReverseIterator      =       T*;
@@ -105,5 +113,17 @@ namespace Oggle
             return { m_Data[Index] };
         }
         return { };
+    }
+
+    template <typename T>
+    size_t Span<T>::Count() const
+    {
+        return m_Count;
+    }
+
+    template <typename T>
+    T* Span<T>::Data() const
+    {
+        return m_Data;
     }
 }
